fa2a7c9ba3638dd075e2ec3ac3d0cc5c48f4fdb0
viniciusarauzx <varaujod08@gmail.com>
2025-08-22 22:16:13 -0300

    Primeira parte

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './core/header/header';
+import { Footer } from "./core/footer/footer";
+import { Banner } from './core/banner/banner';
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, Footer, Banner],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/core/header/header.ts
+++ src/app/core/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header', // <app-header>
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './header.css'
 })
 export class Header {
-
+  @Input() titulo: string = 'Angular Store';
 }
