4448339a77f2df829c6c49e6e7628415ab2d0e2f
viniciusarauzx <varaujod08@gmail.com>
2025-08-22 21:00:30 -0300

    Criação da Loja

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from './core/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('loja-tp1-angular');
+  
 }
