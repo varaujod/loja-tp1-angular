4178674f64a3934d5750feb307f39fcf71308366
viniciusarauzx <varaujod08@gmail.com>
2025-09-05 20:39:40 -0300

    continuacao

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { Produto } from '../../../model/produto';
 import { CardProduto } from "../card-produto/card-produto";
 
@@ -16,21 +16,31 @@ export class ListaProdutos {
     descricao: 'Desc. Produto 1',
     preco: 179.00,
     imageURL: 'images/logoifsp.png',
-    promo: true
+    // promo: true
   },
   {
     id: 2,
     nome: 'Produto 2',
     descricao: 'Desc. Produto 2',
-    preco: 179.00
+    preco: 179.00,
+    estado: 'esgotado'
   },
   {
     id: 3,
     nome: 'Produto 3',
     descricao: 'Desc. Produto 3',
-    preco: 179.00
+    preco: 179.00,
+    estado: 'novo'
   }
   ]
+
+  apenasPromo = signal(false);
+
+  prodExibidos = computed(() => this.apenasPromo() ? this.produtos.filter(p => p.promo) : this.produtos);
+
+  alternarPromo(){
+    this.apenasPromo.update(p => !p);
+  }
 
   onAddProduct(produto: { id: number, quantidade: number }){
     alert(`Produto ${produto.id}, ${produto.quantidade} unidades`);
